0166949909efb82b077559bbe67e5aa1f68af226
John Houston <jhouston@hashicorp.com>
2022-11-09 21:59:37 -0500

    add outputs

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "kubernetes_version" {
+  type = string
+  description = "Cluster Kubernetes version"
+}
+
 resource null_resource example {
   provisioner "local-exec" {
     command = "echo test configuration"
@@ -5,7 +10,18 @@ resource null_resource example {
 }
 
 resource null_resource example2 {
+  depends_on = [
+    null_resource.example
+  ]
   provisioner "local-exec" {
     command = "echo test configuration 2"
   }
+}
+
+output "control_plane_endpoint_host" {
+  value = "localhost"
+}
+
+output "control_plane_endpoint_port" {
+  value = 8000
 }
